28d366c773274a1220234a0a22414ec6349cf7c7
mgriffin-sg <mike.griffin@speech-graphics.com>
2023-03-31 11:37:17 +0100

    get rid of that

--- backends/imgui_impl_sdl.h
+++ backends/imgui_impl_sdl.h
@@ -15,7 +15,7 @@
 // Read online: https://github.com/ocornut/imgui/tree/master/docs
 
 #pragma once
-#include "EventEditor/3rd_party/imgui.h"      // IMGUI_IMPL_API
+#include "imgui.h"      // IMGUI_IMPL_API
 
 struct SDL_Window;
 typedef union SDL_Event SDL_Event;
